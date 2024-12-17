b2d56b578ad03de201f40bb8c78c5d6bc3f23614
AC <66705643+ACwolf55@users.noreply.github.com>
2024-12-17 13:30:51 -0500

    Update Home.vue
    import data from - NFLrankings page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,8 +2,7 @@
 
 import Header from "@/components/Header.vue";
 
-
-import { nflRankings, topDawgs, dawgWatch } from "../data/rankings";
+import { nflRankings, topDawgs, dawgWatch } from '../data/nflRankings';
 
 export default {
   name: "Home",
